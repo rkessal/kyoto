6cc97248e8af7ad445df200a268c5aa54fb073d4
rkessal <kessal.rayhan25@gmail.com>
2025-03-02 15:14:55 +0100

    preloading images

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,34 @@ import Image from "next/image";
 import FancyLink from "./components/fancy-link";
 import MenuLink from "./components/menu-link";
 
+import fs from 'fs';
+import path from 'path';
+import Head from 'next/head';
+
+async function getImages() {
+  const collectionsDir = path.join(process.cwd(), 'public/collections');
+  const collections = fs.readdirSync(collectionsDir);
+
+  let images = []
+
+  collections.forEach((collection) => {
+    const smallDir = path.join(collectionsDir, collection, 'small');
+    const normalDir = path.join(collectionsDir, collection, 'normal');
+
+    if (fs.existsSync(smallDir)) {
+      const smallImages = fs.readdirSync(smallDir).filter(file => file.match(/\d+\.png$/));
+      images.push(...smallImages.map(file => `/collections/${collection}/small/${file}`));
+    }
+
+    if (fs.existsSync(normalDir)) {
+      const normalImages = fs.readdirSync(normalDir).filter(file => file.match(/\d+\.png$/));
+      images.push(...normalImages.map(file => `/collections/${collection}/normal/${file}`));
+    }
+  });
+
+  return images;
+}
+
 const funnelSans = Funnel_Sans({
   variable: "--font-funnel-sans",
   subsets: ["latin"],
@@ -26,28 +54,37 @@ export const metadata = {
   description: "Kyoto",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const images = await getImages();
+
   return (
-    <html lang="en">
-      <body
-        className={`${funnelSans.variable} ${buttacup.variable} antialiased`}
-      >
-        <nav className="z-10 p-8 flex flex-row items-start w-full fixed top-0 left-0  mix-blend-difference text-white">
-          <figure className="invert">
-            <Image src='/logo.svg' height={13} width={67} alt=""/>
-            <span className="text-[0rem]">Kyoto</span>
-          </figure>
-          <ul className="capitalize top-[2rem] left-0 absolute pl-[87.4rem]">
-            <li><MenuLink href='/' active>Home</MenuLink> </li>
-            <li><MenuLink href='/collections'>Shop</MenuLink></li>
-            <li><MenuLink href='/about'>About</MenuLink></li>
-            <li><MenuLink href='#'>journal</MenuLink></li>
-            <li><MenuLink href='/contact'>contact</MenuLink></li>
-          </ul>
-          <div className="ml-auto">Bag (0)</div>
-        </nav>
-        {children}
-      </body>
-    </html>
+    <>
+      <Head>
+        {images.map((src) => (
+          <link key={src} rel="preload" href={src} as="image" />
+        ))}
+      </Head>
+      <html lang="en">
+        <body
+          className={`${funnelSans.variable} ${buttacup.variable} antialiased`}
+        >
+          <nav className="z-10 p-8 flex flex-row items-start w-full fixed top-0 left-0  mix-blend-difference text-white">
+            <figure className="invert">
+              <Image src='/logo.svg' height={13} width={67} alt="" />
+              <span className="text-[0rem]">Kyoto</span>
+            </figure>
+            <ul className="capitalize top-[2rem] left-0 absolute pl-[87.4rem]">
+              <li><MenuLink href='/' active>Home</MenuLink> </li>
+              <li><MenuLink href='/collections'>Shop</MenuLink></li>
+              <li><MenuLink href='/about'>About</MenuLink></li>
+              <li><MenuLink href='#'>journal</MenuLink></li>
+              <li><MenuLink href='/contact'>contact</MenuLink></li>
+            </ul>
+            <div className="ml-auto">Bag (0)</div>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import CollectionsSelectorItem from "./components/collections-selector-item";
-import collections from "./data/collections";
 import CollectionsDetail from "./components/collections-detail";
 import LimitedProposition from "./components/limited-proposition";
 import Link from "next/link";
 import { Suspense } from "react";
 import LenisScroll from "./components/lenis-scroll";
-import Arrow from "./components/arrow";
 import CollectionsSelector from "./components/collections-selector";
 import CollectionsPreview from "./components/collections-preview";
 import CollectionsDescription from "./components/collections-description";
